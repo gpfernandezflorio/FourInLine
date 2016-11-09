deef28ba3ae0921f56fdae37ab422ce03666e10a
gpfernandezflorio <gpfernandezflorio@gmail.com>
2016-11-09 20:10:00 -0300

    q onda?

--- src/fourInLine.py
+++ src/fourInLine.py
@@ -8,53 +8,78 @@ def make_matrix(board, height):
     for j in i:
       m += str(j)
     m+=";"
-  
+  return m
+
+def display_board(board, height):
+    for j in range(height-1,-1,-1):
+        row = "|"
+        for i in range(len(board)):
+            if j < len(board[i]):
+                row += str(board[i][j]) + "|"
+            else:
+                row += " |"
+        print (row)
+    row = " "
+    for i in range(len(board)):
+        row += str(i+1) + " "
+    print row
 
 class FourInLine:
-    def __init__(self, playerX, playerO, width, height):
+    def __init__(self, player1, player2, width, height):
         self.height = height
         self.board = []
         for i in range(width):
             self.board.append([])
-        self.red, self.blue = playerX, playerO
-        self.turn = random.choice(['B', 'Y'])
+        self.red, self.blue = player1, player2
+        self.turn = random.choice(['1', '2'])
 
-    def play_game(self):
-        self.red.start_game('Y',len(self.board))
-        self.blue.start_game('B',len(self.board))
+    def play_game(self, play=False):
+        self.red.start_game('1',len(self.board))
+        self.blue.start_game('2',len(self.board))
         while True: #yolo
-            if self.turn == 'Y':
-                player, char, other_player = self.red, 'Y', self.blue
+            if self.turn == '1':
+                player, char, other_player = self.red, '1', self.blue
             else:
-                player, char, other_player = self.blue, 'B', self.red
+                player, char, other_player = self.blue, '2', self.red
             if player.breed == "human":
-                self.display_board()
+                display_board(self.board, self.height)
             column = player.move(self.board,self.height)
             self.board[column].append(char)
             if self.player_wins(char,column):
-                player.reward(1, self.board,self.height)
+                player.reward(1, self.board, self.height)
                 other_player.reward(-1, self.board,self.height)
-                self.display_board()
+                if play:
+                    display_board(self.board, self.height)
+                    print "Jugador " + self.turn + " GANA! (" + player.breed + ")"
                 break
             if self.board_full(): # tie game
-                player.reward(0.5, self.board,self.height)
+                player.reward(0.5, self.board, self.height)
                 other_player.reward(0.5, self.board,self.height)
-                self.display_board()
+                if play:
+                    display_board(self.board, self.height)
+                    print "EMPATE!"
                 break
             other_player.reward(0, self.board,self.height)
-            if self.turn == 'Y':
-                self.turn = 'B'
+            if self.turn == '1':
+                self.turn = '2'
             else:
-                self.turn = 'Y'
+                self.turn = '1'
+
+    def board_full(self):
+        return not any([len(column) < self.height for column in self.board])
 
     def player_wins(self, char, column):
         pos_y = len(self.board[column])-1
         pos_x = column
-        vertical_check = self.check_vertically(char, pos_x, pos_y)
-        horizontal_check = self.check_horizontally(char, pos_x, pos_y)
-        identity_check = self.check_identity_diagonal(char, pos_x, pos_y)
-        not_identity_check = self.check_not_identity_diagonal(char, pos_x, pos_y)
-        return vertical_check or horizontal_check or identity_check or not_identity_check
+        if(self.check_vertically(char, pos_x, pos_y)):
+            return True
+        if(self.check_horizontally(char, pos_x, pos_y)):
+            return True
+        if(self.check_identity_diagonal(char, pos_x, pos_y)):
+            return True
+        if(self.check_not_identity_diagonal(char, pos_x, pos_y)):
+            return True
+        return False
 
     def check_vertically(self, char, pos_x, pos_y):
         if len(self.board[pos_x]) < 4:
@@ -62,6 +87,7 @@ class FourInLine:
         column = self.board[pos_x]
         if char == column[pos_y-1] == column[pos_y-2] == column[pos_y-3]:
             return True
+        return False
 
     def check_horizontally(self, char, pos_x, pos_y):
         result = False
@@ -78,11 +104,13 @@ class FourInLine:
         #     result = result or check_horizontally_aux(self, char, pos_x, pos_y, 1, 2, 3)
         return result
 
-
     def check_horizontally_aux(self, char, pos_x, pos_y, offset_1, offset_2, offset_3):
-        try: 
+        if (pos_x+offset_1 < 0 or pos_x+offset_2 < 0 or pos_x+offset_3 < 0):
+            return False
+        try:
             if char == self.board[pos_x+offset_1][pos_y] == self.board[pos_x+offset_2][pos_y] == self.board[pos_x+offset_3][pos_y]:
                 return True
+            return False
         except:
             return False
 
@@ -92,11 +120,15 @@ class FourInLine:
             result = result or self.check_identity_diagonal_aux(char, pos_x, pos_y, a, b, c)
         return result
 
-
     def check_identity_diagonal_aux(self, char, pos_x, pos_y, offset_1, offset_2, offset_3):
-        try: 
+        if (pos_x+offset_1 < 0 or pos_x+offset_2 < 0 or pos_x+offset_3 < 0):
+            return False
+        if (pos_y+offset_1 < 0 or pos_y+offset_2 < 0 or pos_y+offset_3 < 0):
+            return False
+        try:
             if char == self.board[pos_x+offset_1][pos_y+offset_1] == self.board[pos_x+offset_2][pos_y+offset_2] == self.board[pos_x+offset_3][pos_y+offset_3]:
                 return True
+            return False
         except:
             return False
 
@@ -106,33 +138,24 @@ class FourInLine:
             result = result or self.check_not_identity_diagonal_aux(char, pos_x, pos_y, a, b, c)
         return result
 
-
     def check_not_identity_diagonal_aux(self, char, pos_x, pos_y, offset_1, offset_2, offset_3):
-        try: 
+        if (pos_x+offset_1 < 0 or pos_x+offset_2 < 0 or pos_x+offset_3 < 0):
+            return False
+        if (pos_y-offset_1 < 0 or pos_y-offset_2 < 0 or pos_y-offset_3 < 0):
+            return False
+        try:
             if char == self.board[pos_x+offset_1][pos_y-offset_1] == self.board[pos_x+offset_2][pos_y-offset_2] == self.board[pos_x+offset_3][pos_y-offset_3]:
                 return True
+            return False
         except:
             return False
-
-    def board_full(self):
-        return not any([len(column) < self.height for column in self.board])
-
-    def display_board(self):
-        for j in range(self.height-1,-1,-1):
-            row = "|"
-            for i in range(len(self.board)):
-                if j < len(self.board[i]):
-                    row += str(self.board[i][j]) + "|"
-                else:
-                    row += " |"
-            print (row)
 
 class Player(object):
     def __init__(self):
         self.breed = "human"
 
     def start_game(self, char, width):
-        print "\nNew game!"
+        pass
 
     def move(self, board, height):
         x = len(board)
@@ -150,7 +173,7 @@ class Player(object):
         return x
 
     def reward(self, value, board, height):
-        print "{} rewarded: {}".format(self.breed, value)
+        pass
 
     def available_moves(self, board, height):
         return [i for i in range(len(board)) if len(board[i]) < height]
@@ -163,7 +186,7 @@ class RandomPlayer(Player):
     def reward(self, value, board, height):
         pass
 
-    def start_game(self, char):
+    def start_game(self, char, width):
         pass
 
     def move(self, board, height):
@@ -172,7 +195,6 @@ class RandomPlayer(Player):
 class QLearningPlayer(Player):
     def __init__(self, epsilon=0.2, alpha=0.3, gamma=0.9):
         self.breed = "Qlearner"
-        self.harm_humans = False
         self.q = {} # (state, action) keys: Q values
         self.epsilon = epsilon # e-greedy chance of random exploration
         self.alpha = alpha # learning rate
@@ -191,14 +213,14 @@ class QLearningPlayer(Player):
         return self.q.get((state, action))
 
     def move(self, board, height):
-        self.last_board = make_matrix(board, height)
+        self.last_board = board
         actions = self.available_moves(board, height)
 
         if random.random() < self.epsilon: # explore!
             self.last_move = random.choice(actions)
             return self.last_move
 
-        qs = [self.getQ(self.last_board, a) for a in actions]
+        qs = [self.getQ(make_matrix(board, height), a) for a in actions]
         maxQ = max(qs)
 
         if qs.count(maxQ) > 1:
@@ -216,28 +238,29 @@ class QLearningPlayer(Player):
             self.learn(self.last_board, self.last_move, value, board, height)
 
     def learn(self, state, action, reward, result_state, height):
-        prev = self.getQ(state, action)
-        maxqnew = max([self.getQ(make_matrix(result_state, height), a) for a in self.available_moves(result_state,height)])
-        self.q[(state, action)] = prev + self.alpha * ((reward + self.gamma*maxqnew) - prev)
+        prev = self.getQ(make_matrix(state, height), action)
+        if len(self.available_moves(result_state, height)) == 0:
+            maxqnew = 0
+        else:
+            maxqnew = max([self.getQ(make_matrix(result_state, height), a) for a in self.available_moves(result_state, height)])
+        self.q[(make_matrix(state, height), action)] = prev + self.alpha * ((reward + self.gamma*maxqnew) - prev)
 
-# p1 = RandomPlayer()
-# p1 = MinimaxPlayer()
-# p1 = MinimuddledPlayer()
-# p1 = QLearningPlayer()
-# p2 = QLearningPlayer()
+p1 = RandomPlayer()
+p1 = QLearningPlayer()
+#epsilon, gamma, alpha
+p2 = QLearningPlayer(0.9,0.3,0.9)
 
+size = (6, 7)
 
-
-#for i in xrange(0,200):
-#    t = TicTacToe(p1, p2)
-#    t.play_game()
-
-#p1 = Player()
-#p2.epsilon = 0
+for i in xrange(0,30000):
+    t = FourInLine(p1, p2, size[0], size[1])
+    t.play_game()
+    if i % 1000 == 0: print i
 
 p1 = Player()
-p2 = QLearningPlayer()
+#p2 = Player()
+p2.epsilon = 0
 
 while True:
-    t = FourInLine(p1, p2, 9, 12)
-    t.play_game()
+    t = FourInLine(p1, p2, size[0], size[1])
+    t.play_game(True)
